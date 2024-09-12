10933a83ec2ac3e201842bec5240d07bd64ff693
trillich <will@serensoft.com>
2024-09-12 11:00:37 -0400

    bit of a restructuring for accomodate course data

--- MCM_GetAccommodateCourseData.sql
+++ MCM_GetAccommodateCourseData.sql
@@ -17,37 +17,38 @@ AS
 -- =============================================
 BEGIN
 
+-- defaults for testing:
 declare @curyr int = 2024;
 declare @cterm char(2) = 'FA';
--- declare @daysago int = 999;
+-- declare @daysago int = 1000;
+select @cterm = dbo.MCM_FN_CALC_TRM('C');
+set @curyr = cast(left(@cterm,4) as int);
+SET @cterm = right(@cterm,2) + left(@cterm,4); -- SSYYYY (not YYYYSS)
 
 with
 cte_sched
 as (
+    -- declare @curyr int = 2024;
+    -- declare @cterm char(2) = 'FA';
     select
         ROW_NUMBER() over (partition by crs_cde order by MONDAY_CDE,TUESDAY_CDE,WEDNESDAY_CDE,THURSDAY_CDE,FRIDAY_CDE) rownum,
-        -- ss.YR_CDE,
-        -- ss.TRM_CDE,
         ss.CRS_CDE,
-        ss.MONDAY_CDE,
-        ss.TUESDAY_CDE,
-        ss.WEDNESDAY_CDE,
-        ss.THURSDAY_CDE,
-        ss.FRIDAY_CDE,
-        ss.SATURDAY_CDE,
-        ss.SUNDAY_CDE,
         replace(ss.SUNDAY_CDE+ss.MONDAY_CDE+ss.TUESDAY_CDE+ss.WEDNESDAY_CDE+ss.THURSDAY_CDE+ss.FRIDAY_CDE+ss.SATURDAY_CDE,' ','-') sched,
-        FORMAT(ss.BEGIN_DTE,'M/d/yyyy')     begin_dte,
-        FORMAT(ss.END_DTE,'M/d/yyyy')       end_dte,
+        FORMAT(ss.BEGIN_DTE, 'M/d/yyyy')    begin_dte,
+        FORMAT(ss.END_DTE,   'M/d/yyyy')    end_dte,
         FORMAT(ss.BEGIN_TIM, 'HHmm')        begin_tim,
-        FORMAT(ss.END_TIM, 'HHmm')          end_tim
+        FORMAT(ss.END_TIM,   'HHmm')        end_tim
     from
         SECTION_SCHEDULES ss with (nolock)
     where
-        ss.YR_CDE = @curyr and
-        ss.TRM_CDE = left(@cterm,2)
+        ss.YR_CDE = @curyr
+    and ss.TRM_CDE = left(@cterm,2)
+    -- and ss.CRS_CDE = 'NUR  2000LL'
+    -- order by 1 desc
 ),
 cte_fac as (
+    -- declare @curyr int = 2024;
+    -- declare @cterm char(2) = 'FA';
     SELECT
         ROW_NUMBER() OVER (PARTITION BY yr_cde,trm_cde,crs_cde ORDER BY instrctr_id_num) rownum
         ,fl.CRS_CDE
@@ -66,30 +67,73 @@ cte_fac as (
         AlternateContactMethod acm with (nolock)
         on fl.INSTRCTR_ID_NUM = acm.ID_NUM and acm.ADDR_CDE = '*EML'
     WHERE
-        fl.YR_CDE = @curyr and fl.TRM_CDE = left(@cterm,2)
+        fl.YR_CDE = @curyr
+    and fl.TRM_CDE = left(@cterm,2)
+    -- and fl.CRS_CDE = 'NUR  2000LL'
+),
+cte_reg_stu
+AS (
+    -- declare @curyr int = 2024;
+    -- declare @cterm char(2) = 'FA';
+    -- declare @daysago int = 999;
+    SELECT
+        sch.id_num,
+        sch.CRS_CDE,
+        replace(sch.CRS_CDE,' ','') course_code,
+        sch.GRADE_CDE,
+        case
+            when sch.TRANSACTION_STS = 'C'
+            then 'Y'
+            else 'N'
+            END                     enrolled,
+        case
+            when sch.TRANSACTION_STS = 'C'
+            then ''
+            else format(sch.DROP_DTE, 'M/d/yyyy')
+            END                     enroll_ended_date,
+        sm.CRS_TITLE,
+        sm.CREDIT_HRS,
+        sm.SUBTERM_CDE,
+        sm.TRM_CDE,
+        sm.YR_CDE,
+        sm.FIRST_BEGIN_DTE,
+        sm.LAST_END_DTE
+    FROM
+        student_crs_hist sch with (nolock)
+        join
+        section_master sm
+        on (sch.CRS_CDE = sm.CRS_CDE and sch.YR_CDE = sm.YR_CDE and sch.TRM_CDE = sm.TRM_CDE)
+    WHERE  sch.stud_div IN ( 'UG', 'GR' )
+        AND sch.YR_CDE = @curyr
+        AND sch.TRM_CDE = left(@cterm,2)
+        AND sch.transaction_sts IN ( 'H', 'C', 'D' )
+        and sch.JOB_TIME > getdate() - @daysago -- the One True Filter
+        -- and sch.CRS_CDE like 'NUR  2000L'
+    -- order by 2,1
 )
+
 SELECT
     trim(
-        replace(sm.crs_title,' ','_') + replace(sch.crs_cde,' ','')
+        replace(rs.crs_title,' ','_') + replace(rs.crs_cde,' ','')
         + '_'
         + left(@cterm,2) + cast(@curyr as char(4))
         -- + case when left(@cterm,2) in ('FA','SP') then '' else sm.SUBTERM_CDE end
-        + sm.SUBTERM_CDE
+        + rs.SUBTERM_CDE
     )                               course_unique_id
-    ,0 student_id -- FIXME
-    ,sm.CRS_TITLE                   course_title
-    ,replace(sch.crs_cde,' ','')    course_code
-    ,sm.CREDIT_HRS                  credit_hours
+    ,rs.id_num                      student_id -- FIXME
+    ,rs.CRS_TITLE                   course_title
+    ,rs.course_code
+    ,rs.CREDIT_HRS                  credit_hours
     ,case
-        when sm.TRM_CDE = 'FA' then 'Fall '
-        when sm.TRM_CDE = 'SP' then 'Spring '
-        when sm.TRM_CDE = 'WI' then 'Winter '
-        when sm.TRM_CDE = 'SU' then 'Summer '
+        when rs.TRM_CDE = 'FA' then 'Fall '
+        when rs.TRM_CDE = 'SP' then 'Spring '
+        when rs.TRM_CDE = 'WI' then 'Winter '
+        when rs.TRM_CDE = 'SU' then 'Summer '
         else '?'
         end
-        + sm.YR_CDE                 semester
-    ,format(sm.FIRST_BEGIN_DTE,'M/d/yyyy')             start_date
-    ,format(sm.LAST_END_DTE,'M/d/yyyy')                end_date
+        + rs.YR_CDE                 semester
+    ,format(rs.FIRST_BEGIN_DTE,'M/d/yyyy')             start_date
+    ,format(rs.LAST_END_DTE,'M/d/yyyy')                end_date
     ,sched1.sched                   days_1
     ,sched1.begin_tim               start_time_1
     ,sched1.end_tim                 end_time_1
@@ -105,9 +149,9 @@ SELECT
     ,fac1.alfaname                  inst_sortname_1
     ,fac1.AlternateContact          instr_email_1
     ,fac1.INSTRCTR_ID_NUM           inst_id_1
-    ,'fixme'                        enrolled
-    ,'fixme'                        enroll_ended_date
-    ,'fixme'                        grade
+    ,rs.enrolled
+    ,rs.enroll_ended_date
+    ,rs.GRADE_CDE                   grade
     ,fac2.FIRST_NAME                inst_fname_2
     ,fac2.LAST_NAME                 inst_lname_2
     ,fac2.fullname                  inst_name_2
@@ -120,34 +164,28 @@ SELECT
     ,fac3.alfaname                  inst_sortname_3
     ,fac3.AlternateContact          instr_email_3
     ,fac3.INSTRCTR_ID_NUM           inst_id_3
-         FROM   student_crs_hist sch
-                join
-                section_master sm
-                on (sch.TRM_CDE = sm.TRM_CDE and sch.YR_CDE = sm.YR_CDE and sch.CRS_CDE = sm.CRS_CDE)
+         FROM   cte_reg_stu rs
                 left join
                 cte_fac fac1
-                on (fac1.CRS_CDE = sch.CRS_CDE and fac1.rownum = 1)
+                on (rs.CRS_CDE = fac1.CRS_CDE and fac1.rownum = 1)
                 left join
                 cte_fac fac2
-                on (fac2.CRS_CDE = sch.CRS_CDE and fac2.rownum = 2)
+                on (rs.CRS_CDE = fac2.CRS_CDE and fac2.rownum = 2)
                 left join
                 cte_fac fac3
-                on (fac3.CRS_CDE = sch.CRS_CDE and fac3.rownum = 3)
+                on (rs.CRS_CDE = fac3.CRS_CDE and fac3.rownum = 3)
                 left join
                 cte_sched sched1
-                on (sm.CRS_CDE = sched1.CRS_CDE and sched1.rownum = 1)
+                on (rs.CRS_CDE = sched1.CRS_CDE and sched1.rownum = 1)
                 left join
                 cte_sched sched2
-                on (sm.CRS_CDE = sched2.CRS_CDE and sched1.rownum = 2)
+                on (rs.CRS_CDE = sched2.CRS_CDE and sched1.rownum = 2)
                 left join
                 cte_sched sched3
-                on (sm.CRS_CDE = sched3.CRS_CDE and sched1.rownum = 3)
-
-         WHERE  sch.stud_div IN ( 'UG', 'GR' )
-                AND sch.YR_CDE = @curyr
-                AND sch.TRM_CDE = left(@cterm,2)
-                AND sch.transaction_sts IN ( 'H', 'C', 'D' )
-                AND sch.job_time > getdate() - @daysago
+                on (rs.CRS_CDE = sched3.CRS_CDE and sched1.rownum = 3)
+where sched2.CRS_CDE > '!'
+        order BY
+            1,2;
 
     set nocount off;
     REVERT
@@ -155,4 +193,3 @@ END
 
 ;
 GO
-
